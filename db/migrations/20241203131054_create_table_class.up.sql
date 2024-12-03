93e0f79f6a5471d068e1950132c5a97f72bbdccf
mortomulu <mfajrihidayat09@gmail.com>
2024-12-03 22:47:09 +0700

    fix: migrate

--- db/migrations/20241203131054_create_table_class.up.sql
+++ db/migrations/20241203131054_create_table_class.up.sql
@@ -10,14 +10,14 @@ $$;
 -- Create table 'kelas' if not exists with a foreign key to 'users' table
 CREATE TABLE IF NOT EXISTS class (
     id SERIAL PRIMARY KEY,
-    creatorclassId UUID NOT NULL,
+    creator_class_id UUID NOT NULL,
     title VARCHAR(255) NOT NULL,
     description TEXT,
     image TEXT,
     video TEXT,
     status visibility NOT NULL DEFAULT 'draf',
-    createdAt TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updatedAt TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
 
     -- Foreign key constraint to 'users' table
     CONSTRAINT fk_creatorclass FOREIGN KEY (creatorclassId) REFERENCES users(id) ON DELETE CASCADE
