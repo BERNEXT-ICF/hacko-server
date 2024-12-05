9801f17927b0a0bafd7a0f00ac87e4c9a851118b
mortomulu <mfajrihidayat09@gmail.com>
2024-12-06 04:11:26 +0700

    feat: create modules

---
+++ internal/module/modules/handler/rest/handler.go
@@ -0,0 +1,76 @@
+package handler
+
+import (
+	"hacko-app/internal/adapter"
+	"hacko-app/internal/middleware"
+	"hacko-app/internal/module/modules/entity"
+	"hacko-app/internal/module/modules/ports"
+	"hacko-app/internal/module/modules/repository"
+	"hacko-app/internal/module/modules/service"
+	"hacko-app/pkg/errmsg"
+	"hacko-app/pkg/response"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/rs/zerolog/log"
+)
+
+type modulesHandler struct {
+	service ports.ModulesService
+}
+
+func NewModulesHandler() *modulesHandler {
+	var handler = new(modulesHandler)
+
+	repo := repository.NewModulesRepository(adapter.Adapters.HackoPostgres)
+	modulesService := service.NewModulesService(repo)
+
+	handler.service = modulesService
+	return handler
+}
+
+
+
+func (h *modulesHandler) Register(router fiber.Router) {
+	router.Post("/class/materials/:materialsId/modules", middleware.AuthMiddleware, middleware.AuthRole([]string{"user", "admin", "teacher"}), h.CreateModules)
+}
+
+func (h *modulesHandler) CreateModules(c *fiber.Ctx) error {
+	var (
+		req = new(entity.CreateModulesRequest)
+		ctx = c.Context()
+		v   = adapter.Adapters.Validator
+		l   = middleware.GetLocals(c)
+	)
+
+	req.UserId = l.GetUserId()
+
+	id := c.Params("materialsId")
+
+	reqId, err := strconv.Atoi(id)
+	if err != nil {
+		log.Warn().Err(err).Msg("handler::CreateMaterials - Failed to parsing id materials")
+		return c.Status(fiber.StatusInternalServerError).JSON(response.Error(errmsg.NewCustomErrors(500, errmsg.WithMessage("Failed to parse params id materials"))))
+	}
+
+	req.MaterialsId = reqId
+
+	if err := c.BodyParser(req); err != nil {
+		log.Warn().Err(err).Msg("handler::CreateMaterials - Failed to parse request body")
+		return c.Status(fiber.StatusBadRequest).JSON(response.Error(err))
+	}
+
+	if err := v.Validate(req); err != nil {
+		log.Warn().Err(err).Msg("handler::CreateMaterials - Invalid request body")
+		code, errs := errmsg.Errors(err, req)
+		return c.Status(code).JSON(response.Error(errs))
+	}
+
+	res, err := h.service.CreateModules(ctx, req)
+	if err != nil {
+		code, errs := errmsg.Errors[error](err)
+		return c.Status(code).JSON(response.Error(errs))
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(response.Success(res, ""))
+}
---
+++ internal/module/modules/service/service.go
@@ -0,0 +1,29 @@
+package service
+
+import (
+	"context"
+	"hacko-app/internal/module/modules/entity"
+	"hacko-app/internal/module/modules/ports"
+)
+
+var _ ports.ModulesService = &modulesService{}
+
+type modulesService struct {
+	repo ports.ModulesRepository
+}
+
+func NewModulesService(repo ports.ModulesRepository) *modulesService {
+	return &modulesService{
+		repo: repo,
+	}
+}
+
+func (s *modulesService) CreateModules(ctx context.Context, req *entity.CreateModulesRequest) (*entity.CreateModulesResponse, error) {
+
+	response, err := s.repo.CreateModules(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
