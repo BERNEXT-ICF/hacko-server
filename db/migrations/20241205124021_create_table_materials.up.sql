aea62f8d8cc777c89aeee29c1cfc6220dffe345d
mortomulu <mfajrihidayat09@gmail.com>
2024-12-05 19:54:47 +0700

    migrate: materials table

---
+++ db/migrations/20241205124021_create_table_materials.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS materials (
+    id SERIAL PRIMARY KEY, -- Kolom ID dengan auto increment
+    user_id UUID NOT NULL,
+    title VARCHAR(255) NOT NULL, -- Kolom untuk judul material
+    class_id INT NOT NULL, -- Kolom untuk ID kelas, foreign key ke tabel `classes`
+    users_classes_id INT NOT NULL, -- Kolom untuk ID user kelas, foreign key ke tabel `users_classes`
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, -- Kolom waktu pembuatan
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, -- Kolom waktu pembaruan
+    FOREIGN KEY (class_id) REFERENCES class(id) ON DELETE CASCADE, -- Foreign key ke tabel `classes`
+    FOREIGN KEY (users_classes_id) REFERENCES users_classes(id) ON DELETE CASCADE -- Foreign key ke tabel `users_classes`
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE -- Foreign key ke tabel `users`
+);
