2d330e7eb989fe930fbd28c36914bf7cca1b423e
mortomulu <mfajrihidayat09@gmail.com>
2024-12-06 04:10:05 +0700

    migrate: update modules table

--- db/migrations/20241205175351_create_modules_table.up.sql
+++ db/migrations/20241205175351_create_modules_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS modules (
     id SERIAL PRIMARY KEY, -- Kolom ID dengan auto increment
-    creator_materials_id UUID NOT NULL,
+    creator_modules_id UUID NOT NULL,
     materials_id INT NOT NULL, --Kolom 
     title VARCHAR(255) NOT NULL, -- Kolom untuk judul material
     content TEXT, -- Kolom untuk judul material
@@ -8,6 +8,6 @@ CREATE TABLE IF NOT EXISTS modules (
     videos TEXT[], -- Array string untuk video
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, -- Kolom waktu pembuatan
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, -- Kolom waktu pembaruan
-    FOREIGN KEY (creator_materials_id) REFERENCES users(id) ON DELETE CASCADE, -- Foreign key ke tabel `users`
+    FOREIGN KEY (creator_modules_id) REFERENCES users(id) ON DELETE CASCADE, -- Foreign key ke tabel `users`
     FOREIGN KEY (materials_id) REFERENCES materials(id) ON DELETE CASCADE -- Foreign key ke tabel `users`
 );
