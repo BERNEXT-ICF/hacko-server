ded041252b94dd03b0830f6e9bc3bb507ce98d70
mortomulu <mfajrihidayat09@gmail.com>
2024-12-09 17:37:36 +0700

    feat: get all quiz

--- internal/module/quiz/service/service.go
+++ internal/module/quiz/service/service.go
@@ -33,13 +33,27 @@ func (s *quizService) CreateQuiz(ctx context.Context, req *entity.CreateQuizRequ
 	return response, nil
 }
 
-func (s *quizService) CreateQuestionQuiz(ctx context.Context, req *entity.CreateQuestionQuizRequest) (*entity.CreateQuestionQuizResponse, error){
+func (s *quizService) CreateQuestionQuiz(ctx context.Context, req *entity.CreateQuestionQuizRequest) (*entity.CreateQuestionQuizResponse, error) {
 	err := s.repo.FindQuiz(ctx, req.QuizId)
 	if err != nil {
 		return nil, err
 	}
 
 	response, err := s.repo.CreateQuestionQuiz(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func (s *quizService) GetAllQuiz(ctx context.Context, req *entity.GetAllQuizRequest) ([]entity.GetAllQuizResponse, error) {
+	err := s.repo.FindClass(ctx, req.ClassId)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := s.repo.GetAllQuiz(ctx, req)
 	if err != nil {
 		return nil, err
 	}
